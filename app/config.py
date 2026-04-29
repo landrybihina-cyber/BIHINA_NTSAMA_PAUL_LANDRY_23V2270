00463ae36a9cef2fcd31ef4a2f2f752a5e534b7a
landrybihina-cyber <landry.bihina@facsiences.cm>
2026-04-29 16:52:00 +0100

    rgrgrg

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     # Utilise une base SQLite locale pour le développement
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///students.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/students.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
