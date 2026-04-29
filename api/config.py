5eb2469796c1c36a9c0226a6a7933f61b5cfa540
landrybihina-cyber <landrybihina@facsciences-uy1.cm@exemple.com>
2026-04-29 14:25:49 +0100

    Premier commit

--- api/config.py
+++ api/config.py
@@ -2,6 +2,5 @@ import os
 
 
 class Config:
-    SECRET_KEY = 'ma_cle_secrete_2026'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///students.db'      # CORR: espace supprimé après 'sqlite:'
     SQLALCHEMY_TRACK_MODIFICATIONS = False                  # CORR: 'MODIFATIONS' → 'MODIFICATIONS'
